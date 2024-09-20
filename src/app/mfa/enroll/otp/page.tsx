09f9c451ca7cf9a5fc231b07fadffc18eca5fc0a
Sho Fujita <sho.fujita@okta.com>
2024-09-20 21:45:11 +0900

    Fixed production build issue

--- src/app/mfa/enroll/otp/page.tsx
+++ src/app/mfa/enroll/otp/page.tsx
@@ -1,7 +1,7 @@
 import EnrollAPIResponse from "./EnrollAPIResponse"
 import VerifyAPIResponse from "./VerifyAPIResponse"
 
-export default async function enrollWithOTP(uri: string) {
+export default async function enrollWithOTP() {
   return (
     <>
       <EnrollAPIResponse />
