135425386b67a77fed9a3ec622a70f2ff3301b4f
Sho Fujita <sho.fujita@okta.com>
2024-10-10 13:53:33 +0900

    Logging endpoint

--- src/app/api/log/route.ts
+++ src/app/api/log/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export const POST = async (req: NextRequest) => {
-  console.log("/api/log", await req.json())
+  console.log("/api/log", await req.text())
   return NextResponse.json(
     {
       message: "OK",
